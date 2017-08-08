2fc1777087faece5d46f6411bd9b9fcb22cb5260
tuanlung <tuanlung@g.ucla.edu>
2017-08-08 14:34:03 -0400

    Initial Commit

---
+++ TipWorks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipWorks
+//
+//  Created by Tuan-Lung Wang on 8/8/17.
+//  Copyright © 2017 Tuan-Lung Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
