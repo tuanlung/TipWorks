42e62259de43c7dd957628505ed7306018bf1098
tuanlung <tuanlung@g.ucla.edu>
2017-08-08 15:22:00 -0400

    feat: initialize numbersView's frame according to keyboard and navigationBar positions

--- TipWorks/CalculatorViewController.swift
+++ TipWorks/CalculatorViewController.swift
@@ -9,21 +9,50 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
+    
+    @IBOutlet weak var numbersView: UIView!
+    @IBOutlet weak var billTextField: UITextField!
 
+    var keyboardMinY: CGFloat = 0.0
+    var navigationBarMaxY: CGFloat = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // make bill text field first responder
-        // getKeyboardMinY
-        // initializePositionOfAllViews
-
+        subscribeToKeyboardEvents()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        billTextField.becomeFirstResponder()        
+    }
+    
+    
+    func initializePositionOfAllViews() {
+        let newHeight = keyboardMinY - navigationBarMaxY
+        numbersView.frame = CGRect(x: 0.0, y: navigationBarMaxY, width: view.frame.width, height: newHeight)
+        print("numbersView scaled to \(numbersView.frame)")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func subscribeToKeyboardEvents() {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardDidShow(_:)), name: .UIKeyboardDidShow , object: nil)
     }
-
-
+    
+    func initializePositionFactors() {
+        navigationBarMaxY = view.frame.minY + navigationController!.navigationBar.frame.height
+    }
+    
+    
+    // MARK: call back functions
+    func keyboardDidShow(_ notification: NSNotification) {
+        
+        self.keyboardMinY = view.frame.height - (notification.userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue.height
+        
+        initializePositionFactors()
+        initializePositionOfAllViews()
+        
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardDidShow, object: nil)
+    }
 }
 
