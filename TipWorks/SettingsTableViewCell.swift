f1ace033934c6e4e742da1f63da7a86fde612c75
tuanlung <tuanlung@g.ucla.edu>
2017-08-11 10:56:53 -0400

    feat: develop language picking mechanism

---
+++ TipWorks/SettingsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsTableViewCell.swift
+//  TipWorks
+//
+//  Created by Tuan-Lung Wang on 8/11/17.
+//  Copyright © 2017 Tuan-Lung Wang. All rights reserved.
+//
+
+import UIKit
+
+class tipPercTableViewCell: UITableViewCell {
+    @IBOutlet weak var currentSelectionLabel: UILabel!
+}
+
+class maxNumberToSplitTableViewCell: UITableViewCell {
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var currentValueLabel: UILabel!
+}
+
+class languageTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var currentSelectionLabel: UILabel!
+}
--- TipWorks/SettingsViewController.swift
+++ TipWorks/SettingsViewController.swift
@@ -10,42 +10,97 @@ import UIKit
 
 class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIPickerViewDelegate, UIPickerViewDataSource {
     
+
     @IBOutlet weak var languagePickerView: UIPickerView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet var tapRecognizer: UITapGestureRecognizer!
+    
+    
+    
+    
+    var languageIndex = 0
+    var maxNumberToSplit = 10
+
+
+    
+    let tableViewCellIdentifiers = ["tipPercTableViewCell", "maxNumberToSplitTableViewCell", "languageTableViewCell"]
+    let languageOptions = ["English", "español", "中文", "日本語"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         languagePickerView.isHidden = true
+        tapRecognizer.isEnabled = false
     }
     
+    // MARK: picker view delegate functions
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 4
+        return languageOptions.count
     }
     
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return "good"
+        return languageOptions[row]
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        languageIndex = row
+    }
+    
+    // MARK: table view delegate functions
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let row = indexPath.row
-        if row == 2 {
+    
+        if tableViewCellIdentifiers[indexPath.row] == "languageTableViewCell" {
             languagePickerView.isHidden = false
+            tapRecognizer.isEnabled = true
         }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "tipPercTableViewCell")!
-        return cell
+        
+        let identifier = tableViewCellIdentifiers[indexPath.row]
+        
+        let reuseCell = tableView.dequeueReusableCell(withIdentifier: identifier)!
+        
+        
+        switch identifier {
+            case "tipPercTableViewCell":
+                let cell = reuseCell as! tipPercTableViewCell
+                cell.currentSelectionLabel.text = "placeholder"
+            
+            case "maxNumberToSplitTableViewCell":
+                let cell = reuseCell as! maxNumberToSplitTableViewCell
+                //cell.currentValueLabel.text = "\(maxNumberToSplit)"
+                print("placeholder")
+            
+            case "languageTableViewCell":
+                let cell = reuseCell as! languageTableViewCell
+                cell.currentSelectionLabel.text = languageOptions[languageIndex]
+
+            default:
+                print("placeholder")
+
+        }
+        
+        return reuseCell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return tableViewCellIdentifiers.count
     }
+    
+    
+    // MARK: IBActions
+    @IBAction func tappedOnTableView(_ sender: Any) {
+        languagePickerView.isHidden = true
+        tapRecognizer.isEnabled = false
+        tableView.reloadData()
+    }
+
     
 }
 
