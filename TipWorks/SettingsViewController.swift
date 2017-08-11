a4861b0565beec0886a00f6bb792e2aaa1de6d64
tuanlung <tuanlung@g.ucla.edu>
2017-08-10 20:49:15 -0400

    feat: add settings view controller

---
+++ TipWorks/SettingsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsViewController.swift
+//  TipWorks
+//
+//  Created by Tuan-Lung Wang on 8/10/17.
+//  Copyright © 2017 Tuan-Lung Wang. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+}
+
+
