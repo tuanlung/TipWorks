bc0e975686fef901e5ef67f84f88f279631f454b
tuanlung <tuanlung@g.ucla.edu>
2017-08-10 22:18:33 -0400

    feat: implement skeleton for SettingViewController

--- TipWorks/SettingsViewController.swift
+++ TipWorks/SettingsViewController.swift
@@ -8,7 +8,44 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var languagePickerView: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        languagePickerView.isHidden = true
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 4
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "good"
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let row = indexPath.row
+        if row == 2 {
+            languagePickerView.isHidden = false
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tipPercTableViewCell")!
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
     
 }
 
